f18d63eaa28b13a28f59305dcf1dbf7e260ea8cc
Steven <skraus@udel.edu>
2023-03-08 23:29:26 -0500

    commit 2

--- src/nested.ts
+++ src/nested.ts
@@ -154,11 +154,11 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    const newArray = questions.filter(
+    const array2 = questions.filter(
         (question: Question): boolean =>
             question.type === "multiple_choice_question"
     );
-    if (newArray.length === 0 || newArray.length === questions.length) {
+    if (array2.length === 0 || array2.length === questions.length) {
         return true;
     } else {
         return false;
