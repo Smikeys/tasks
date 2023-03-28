c87a6a89add48a2eb98bc5b06ddb8e36b7b79101
Steven <skraus@udel.edu>
2023-03-27 22:24:28 -0400

    commit

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [text, setText] = useState<string>("");
+
+    function updateText(event: React.ChangeEvent<HTMLInputElement>) {
+        const text = event.target.value;
+        setText(text);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <div>Expected Answer: {expectedAnswer}</div>
+            <Form.Group>
+                <Form.Label>
+                    Correct? {expectedAnswer === text ? "✔️" : "❌"}
+                </Form.Label>
+                <Form.Control value={text} onChange={updateText}></Form.Control>
+            </Form.Group>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<string>("3");
+    const [request, setRequest] = useState<string>("");
+    const attemptsToNum = parseInt(attempts) || 0;
+    const requestsToNum = parseInt(request) || 0;
+
+    function updateText(event: React.ChangeEvent<HTMLInputElement>) {
+        const text = event.target.value;
+        setRequest(text);
+    }
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>Attempts Left: {attempts}</div>
+            <Form.Group>
+                <Form.Control
+                    type="number"
+                    value={request}
+                    onChange={updateText}
+                ></Form.Control>
+            </Form.Group>
+            <Button
+                onClick={() => setAttempts((attemptsToNum - 1).toString())}
+                disabled={attemptsToNum === 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() =>
+                    setAttempts((attemptsToNum + requestsToNum).toString())
+                }
+            >
+                gain
+            </Button>
         </div>
     );
 }
